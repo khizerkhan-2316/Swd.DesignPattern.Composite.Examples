9e62e9a1e6785bd89734147566a3e4cc0978aac3
andr522g <166645177+andr522g@users.noreply.github.com>
2025-03-27 15:08:28 +0100

    Added simple version

---
+++ Swd.DesignPattern.Composite.Examples.SalesSimpleExample/Interfaces/ISalesItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swd.DesignPattern.Composite.Examples.SalesSimpleExample.Interfaces
+{
+    public interface ISalesItem
+    {
+        decimal GetPrice();
+        void Display();
+    }
+}
